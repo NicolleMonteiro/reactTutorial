79799a784ae15c5b405f76dbc6fb610d72fcf4e3
Nicole Monteiro Torres <duq0104759@aluno.firjansenaisesi.com.br>
2026-02-27 16:32:38 -0300

    modificações

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Header } from './components/Header/Header.jsx'
 import {Sobre} from './components/Sobre/Sobre.jsx'
+import {Contato} from './components/Contato/Contato.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Header />
     <App />
     <Sobre categoria="sobreMim"/>
+    <Contato categoria="contato"/>
   </React.StrictMode>
 
 )
