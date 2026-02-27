ba6ec0f3db1e6c9d67eba0abd8a5eb29317caf1a
Nicole Monteiro Torres <duq0104759@aluno.firjansenaisesi.com.br>
2026-02-27 15:53:42 -0300

    alterações

--- src/components/Sobre/Sobre.jsx
+++ src/components/Sobre/Sobre.jsx
@@ -7,11 +7,21 @@ export const Sobre = ({categoria}) => {
                 <h2>Sobre min</h2>
 
                 <p>
-                Sou Nicolle Monteiro, desenvolvedora web apaixonada por tecnologia e criação digital.
+                Sou Nicolle Monteiro, desenvolvedora web apaixonada por tecnologia e criação digital. <br /><br />
                 Trabalho com HTML, CSS, JavaScript e React, transformando ideias em sites modernos, funcionais e bem estruturados. 
                 Estou sempre em busca de novos aprendizados para evoluir como profissional e criar experiências cada vez melhores na web.
                 </p>
 
+            </div>
+
+            <div id="meus-conhecimentos">
+                <h2>Meus conhecimentos</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>ReactJS</li>
+                </ul>
             </div>
 
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Header } from './components/Header/Header.jsx'
+import {Sobre} from './components/Sobre/Sobre.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
     <App />
+    <Sobre categoria="sobreMim"/>
   </React.StrictMode>
 
 )
